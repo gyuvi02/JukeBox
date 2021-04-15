c8335f971a4571218da5c11991299a29be58fdbd
Gyula Szabo <44570623+gyuvi02@users.noreply.github.com>
2021-04-15 13:24:41 +0200

    DataListBox class added

--- jukebox.py
+++ jukebox.py
@@ -19,4 +19,58 @@ class Scrollbox(tkinter.Listbox):
         self.scrollbar.grid(row=row, column=column, sticky='nse', rowspan=rowspan)
         self['yscrollcommand'] = self.scrollbar.set
 
+class DataListBox(Scrollbox):
+
+    def __init__(self, window, connection, table, field, sort_order=(), **kwargs):
+        # Scrollbox.__init__(self, window, **kwargs)  # Python 2
+        super().__init__(window, **kwargs)
+
+        self.linked_box = None
+        self.link_field = None
+
+        self.cursor = connection.cursor()
+        self.table = table
+        self.field = field
+
+        self.bind('<<ListboxSelect>>', self.on_select)
+
+        self.sql_select = "SELECT " + self.field + ", _id" + " FROM " + self.table
+        if sort_order:
+            self.sql_sort = " ORDER BY " + ','.join(sort_order)
+        else:
+            self.sql_sort = " ORDER BY " + self.field
+
+    def clear(self):
+        self.delete(0, tkinter.END)
+
+    def link(self, widget, link_field):
+        self.linked_box = widget
+        widget.link_field = link_field
+
+    def requery(self, link_value=None):
+        if link_value and self.link_field:
+            sql = self.sql_select + " WHERE " + self.link_field + "=?" + self.sql_sort
+            print(sql)      # TODO delete this line
+            self.cursor.execute(sql, (link_value,))
+        else:
+            print(self.sql_select + self.sql_sort)      # TODO delete this line
+            self.cursor.execute(self.sql_select + self.sql_sort)
+
+        # clear the listbox contents before re-loading
+        self.clear()
+        for value in self.cursor:
+            self.insert(tkinter.END, value[0])
+
+        if self.linked_box:
+            self.linked_box.clear()
+
+    def on_select(self, event):
+        if self.linked_box:
+            print(self is event.widget)     # TODO delete this line
+            index = self.curselection()[0]
+            value = self.get(index),
+
+            # get the artist ID from the database row
+            link_id = self.cursor.execute(self.sql_select + " WHERE " + self.field + "=?", value).fetchone()[1]
+            self.linked_box.requery(link_id)
 
